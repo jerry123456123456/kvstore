b33f97234aed21ba1ae36923197e70969832d0f3
jerry <songqy2004@qq.com>
2025-04-20 15:27:56 +0800

    1

---
+++ kvstore/Ntyco/sample/nty_mysql_client.c
@@ -0,0 +1,42 @@
+#include "nty_coroutine.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <mysql.h>
+
+
+void func (void *arg) {
+
+	
+	MYSQL* m_mysql = mysql_init(NULL);
+	if (!m_mysql) {
+		printf("mysql_init failed\n");
+		return ;
+	}
+
+	if (!mysql_real_connect(m_mysql, 
+                "192.168.186.138", "jerry", "123456",
+                "JERRY_DB", 3306,
+                NULL, CLIENT_FOUND_ROWS)) {
+		printf("mysql_real_connect failed: %s\n", mysql_error(m_mysql));
+		return ;
+	} else{
+		printf("mysql_real_connect success\n");
+	}
+
+	
+}
+
+int main() {
+#if 1
+	init_hook();
+
+	nty_coroutine *co = NULL;
+	nty_coroutine_create(&co, func, NULL);
+	nty_schedule_run(); //run
+#else
+
+	func(NULL);
+
+#endif
+}
